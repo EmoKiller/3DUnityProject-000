a8b7349b421314d336effba4f648ad1cfd1f073c
ADMIN\Admin <emokiller22101993@gmail.com>
2023-08-28 00:04:14 +0700

    no message

--- Assets/Script/Camera/CameraFollow.cs
+++ Assets/Script/Camera/CameraFollow.cs
@@ -5,45 +5,32 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform playerTransf;
+    [SerializeField] private Transform target;
     [SerializeField] private PlayerControllerPC player;
     [SerializeField] private Vector3 offset = new Vector3(0,9.7f, -13.3f);
     public float smooth;
-    public bool endMove = false;
-    public float timeCount = 0;
+    
     private Vector3 vecref = Vector3.zero;
     // Start is called before the first frame update
     private void Awake()
     {
-        playerTransf = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerPC>();
         transform.eulerAngles = new Vector3(37, 0, 0);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
-        
-    }
-    
-    private void LateUpdate()
-    {
-        transform.position = Vector3.SmoothDamp(transform.position, playerTransf.transform.position + offset, ref vecref, smooth);
+        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset, ref vecref, smooth);
         if (player.isRolling || player.isJump)
         {
-            smooth += Time.deltaTime * 1.1f;
-            timeCount = 1.2f;
-            endMove = false;
+            smooth += Time.deltaTime * 0.4f;
         }
-        if (!endMove)
+        else 
         {
-            smooth -= Time.deltaTime * 0.5f;
-            timeCount -= Time.deltaTime;
-            if (timeCount <= 0)
+            smooth -= Time.deltaTime * 0.16f;
+            if (smooth <= 0)
             {
-                smooth = 0.001f;
-                endMove = true;
+                smooth = 0;
             }
         }
     }
--- Assets/Script/Controller/PlayerControllerPC.cs
+++ Assets/Script/Controller/PlayerControllerPC.cs
@@ -13,14 +13,15 @@ public class PlayerControllerPC : MonoBehaviour
     [SerializeField] private float smoothTime = 0.01f;
     [SerializeField] private float mutiRun = 1f;
     //Rolling
-    [SerializeField] private float CounttimeStopRoll = 0f;
-    [SerializeField] private float timeStopRoll = 0.8f;
+    //[SerializeField] private float CounttimeStopRoll = 0f;
+    [SerializeField] private float timeCountRoll = 0.8f;
     [SerializeField] private float powerPushRoll = 200f;
     //jump
-    [SerializeField] private float timeCountJump = 1f;
+    [SerializeField] private float timeCountJump = 0.5f;
     public bool isRolling = false;
     public bool isJump = false;
     public bool canJump = true;
+    public bool canRolling = true;
     public float horizontal { get; set; }
     public float vertical { get; set; }
     public bool isAlive { get; private set; }
@@ -28,7 +29,7 @@ public class PlayerControllerPC : MonoBehaviour
     private bool horizontalDown => horizontal != 0;
     private bool verticalDown => vertical != 0;
     private Vector3 refVelocity = Vector3.zero;
-    private Vector3 targetVelocity = Vector3.zero;
+    private Vector3 targetVelocity;
     [SerializeField] private Vector3 targetmove;
     [SerializeField] private HeroAnimatorController ani = null;
     [SerializeField] private Rigidbody rb = null;
@@ -64,8 +65,8 @@ public class PlayerControllerPC : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         
-        
-        if (!isJump)
+        //Run
+        if (!isJump || !isRolling)
         {
             mutiRun = Input.GetKey(KeyCode.LeftShift) && (horizontal != 0 || vertical != 0) ? 1.5f : 1f;
         }
@@ -91,7 +92,7 @@ public class PlayerControllerPC : MonoBehaviour
             ani.Rolling();
             if (isRolling)
                 return;
-            Debug.Log("Roll Space Down");
+            timeCountRoll = 0.8f;
             isRolling = true;
 
             
@@ -101,10 +102,9 @@ public class PlayerControllerPC : MonoBehaviour
             if (!canJump)
                 return;
             isJump = true;
-            timeCountJump = 1f;
+            timeCountJump = 0.3f;
             Jump();
             canJump = false;
-            Debug.Log("Roll Space Down");
             
         }
         
@@ -118,48 +118,57 @@ public class PlayerControllerPC : MonoBehaviour
         ani.SetAnimationMove(MathF.Abs(horizontal), MathF.Abs(vertical));
         
     }
-    private void Attack()
-    {
-        transform.DOMove(transform.position + targetmove, 0.2f);
-    }
-    private void Move()
-    {
-        if (horizontalDown || verticalDown)
-        {
-            targetmove = targetVelocity.normalized;
-        }
-        if (!isRolling)
-        {
-            targetVelocity = new Vector3(horizontal,0, vertical) * moveSpeed * mutiRun;
-            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref refVelocity, smoothTime);
-        }
-        
-    }
-    private void Jump()
+    private void TargetMoveZero()
     {
         if (!horizontalDown && !verticalDown)
         {
             targetmove = Vector3.zero;
         }
-        transform.DOJump(transform.localPosition + targetmove * 1.5f , 1f, 1, 0.3f);
     }
+    private void TargetMoveNomal()
+    {
+        if (horizontalDown || verticalDown)
+        {
+            targetmove = targetVelocity.normalized;
+        }
+    }
+    private void Attack()
+    {
+        TargetMoveZero();
+        transform.DOMove(transform.position + targetmove, 0.2f);
+    }
+    private void Jump()
+    {
+        TargetMoveZero();
+        //transform.DOJump(transform.localPosition + targetmove * 1.5f, 1f, 1, 0.3f).SetEase(Ease.InSine);
+        rb.DOJump(transform.localPosition + targetmove * 1.5f, 1f, 1, 0.3f).SetEase(Ease.OutSine);
+    }
+    private void Move()
+    {
+        TargetMoveNomal();
+        if (!isRolling)
+        {
+            targetVelocity = new Vector3(horizontal,0, vertical) * moveSpeed * mutiRun;
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref refVelocity, smoothTime);
+            
+        }
+        
+    }
+    
     private void Roll()
     {
-        
         if (isRolling)
         {
+            timeCountRoll -= Time.deltaTime;
             
-            CounttimeStopRoll += Time.deltaTime;
-            
-            if (CounttimeStopRoll <= timeStopRoll)
+            if (timeCountRoll >= 0)
             {
                 rb.AddForce(Vector3.SmoothDamp(targetmove, targetmove * powerPushRoll, ref refVelocity, 0.2f));
-
             }
             else
             {
                 isRolling = false;
-                CounttimeStopRoll = 0;
+                timeCountRoll = 0;
             }
         }
     }
@@ -185,20 +194,20 @@ public class PlayerControllerPC : MonoBehaviour
         {
             Debug.Log("on Ground");
             isJump = false;
+            
         }
     }
     private void OnCollisionStay(Collision collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
-            if (canJump)
-                return;
-            timeCountJump -= Time.deltaTime;
+            if (timeCountJump > 0)
+                timeCountJump -= Time.deltaTime;
             if (timeCountJump <= 0)
             {
                 canJump = true;
-                
             }
+            //StartCoroutine(TimeCountDown(canJump, 0.5f));
         }
     }
     private void OnCollisionExit(Collision collision)
@@ -208,5 +217,12 @@ public class PlayerControllerPC : MonoBehaviour
             
             
         }
+    }
+    IEnumerator TimeCountDown(bool can,float time)
+    {
+        
+        yield return new WaitForSeconds(time);
+        can = true;
+        
     }
 }
